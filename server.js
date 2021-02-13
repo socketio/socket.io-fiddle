51d60961d6176c58ef6a10865c2acb1673e60a61
Damien Arrachequesne <damien.arrachequesne@gmail.com>
2021-02-13 10:00:16 +0100

    add example from filesystem

--- server.js
+++ server.js
@@ -2,13 +2,22 @@
 const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
-const io = require("socket.io")(server);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "null"
+  }
+});
 const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + "/public"));
 
 io.on("connection", socket => {
   console.log(`connect ${socket.id}`);
+
+  socket.on("ping", (cb) => {
+    console.log("ping received");
+    cb();
+  });
 
   socket.on("disconnect", (reason) => {
     console.log(`disconnect ${socket.id} due to ${reason}`);
