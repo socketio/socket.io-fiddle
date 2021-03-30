af5711c41fc04c74f08a2fdeea72c9900d74e477
Damien Arrachequesne <damien.arrachequesne@gmail.com>
2021-03-30 10:11:00 +0200

    test: add example
    Related: https://github.com/socketio/socket.io/issues/3862

--- server.js
+++ server.js
@@ -10,9 +10,21 @@ app.use(express.static(__dirname + "/public"));
 io.on("connection", socket => {
   console.log(`connect ${socket.id}`);
 
+  socket.join(socket.handshake.query.token);
+  socket.join(socket.handshake.query.token2);
+
+  console.log(socket.rooms); // prints Set { '80kbwCCnnMQBHCuWAAAB', '123', "'456'" }
+
   socket.on("disconnect", (reason) => {
     console.log(`disconnect ${socket.id} due to ${reason}`);
   });
 });
 
 server.listen(port, () => console.log(`server listening at http://localhost:${port}`));
+
+require("socket.io-client")("http://localhost:3000", {
+  query: {
+    token: "123",
+    token2: "'456'"
+  }
+});
