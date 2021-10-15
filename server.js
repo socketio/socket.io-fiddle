e9420aa01b45823e9d7533f328130924ef0333f4
Damien Arrachequesne <damien.arrachequesne@gmail.com>
2021-10-15 08:57:00 +0200

    add example
    Related: https://github.com/socketio/socket.io/issues/4120

--- server.js
+++ server.js
@@ -2,7 +2,9 @@
 const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
-const io = require("socket.io")(server);
+const io = require("socket.io")(server, {
+  allowEIO3: true
+});
 const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + "/public"));
