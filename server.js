1a033be3c9cdbd33fb90d9e58ad8b6d2a71e83dc
Damien Arrachequesne <damien.arrachequesne@gmail.com>
2023-01-29 08:19:26 +0100

    docs: add example
    Related: https://github.com/socketio/socket.io-mongo-adapter/issues/11

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 import { default as express } from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import { createAdapter } from "@socket.io/mongo-adapter";
+import { MongoClient } from "mongodb";
+
+const DB = "mydb";
+const COLLECTION = "socket.io-adapter-events";
+
+const mongoClient = new MongoClient("mongodb://mongo:27017/?replicaSet=rs0");
+
+await mongoClient.connect();
+
+try {
+  await mongoClient.db(DB).createCollection(COLLECTION, {
+    capped: true,
+    size: 1e6
+  });
+} catch (e) {
+  // collection already exists
+}
+const mongoCollection = mongoClient.db(DB).collection(COLLECTION);
 
 const app = express();
 const httpServer = createServer(app);
-const io = new Server(httpServer, {});
+const io = new Server(httpServer, {
+  adapter: createAdapter(mongoCollection)
+});
 
 const port = process.env.PORT || 3000;
 
@@ -21,3 +42,8 @@ io.on("connection", (socket) => {
 httpServer.listen(port, () => {
   console.log(`server listening at http://localhost:${port}`);
 });
+
+setInterval(async () => {
+  const sockets = await io.fetchSockets();
+  console.log("fetchSockets()", sockets.length);
+}, 5000);
