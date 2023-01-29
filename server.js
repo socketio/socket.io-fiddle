516bff852044a8822cd2081bdecccdf22e077f59
Damien Arrachequesne <damien.arrachequesne@gmail.com>
2023-01-29 07:56:22 +0100

    docs: add example
    Related: https://github.com/socketio/socket.io/issues/3703

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import { default as express } from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import { randomBytes } from "crypto";
 
 const app = express();
 const httpServer = createServer(app);
@@ -10,7 +11,11 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
+io.engine.generateId = () => randomBytes(8).toString("hex");
+
 io.on("connection", (socket) => {
+  console.log("Engine.IO id", socket.conn.id);
+  console.log("Socket.IO id", socket.id);
   console.log(`connect ${socket.id}`);
 
   socket.on("disconnect", (reason) => {
