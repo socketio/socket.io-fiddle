fd1f1569d6f9a48f024d089cc72e1f3879e2f5aa
Damien Arrachequesne <damien.arrachequesne@gmail.com>
2023-06-14 08:07:29 +0200

    feat: add example with @socket.io/sticky

--- server.js
+++ server.js
@@ -1,23 +1,56 @@
 import { default as express } from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import cluster from "cluster";
+import { setupMaster, setupWorker } from "@socket.io/sticky";
+import { createClient } from "redis";
+import { createAdapter } from "@socket.io/redis-adapter";
 
-const app = express();
-const httpServer = createServer(app);
-const io = new Server(httpServer, {});
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
 
-const port = process.env.PORT || 3000;
+  const httpServer = createServer();
 
-app.use(express.static("public"));
-
-io.on("connection", (socket) => {
-  console.log(`connect ${socket.id}`);
-
-  socket.on("disconnect", (reason) => {
-    console.log(`disconnect ${socket.id} due to ${reason}`);
+  setupMaster(httpServer, {
+    loadBalancingMethod: "least-connection", // either "random", "round-robin" or "least-connection"
   });
-});
 
-httpServer.listen(port, () => {
-  console.log(`server listening at http://localhost:${port}`);
-});
+  const port = process.env.PORT || 3000;
+
+  httpServer.listen(port, () => {
+    console.log(`server listening at http://localhost:${port}`);
+  });
+
+  for (let i = 0; i < 3; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker) => {
+    console.log(`Worker ${worker.process.pid} died`);
+    cluster.fork();
+  });
+} else {
+  console.log(`Worker ${process.pid} started`);
+
+  const app = express();
+  const httpServer = createServer(app);
+  const io = new Server(httpServer, {});
+
+  app.use(express.static("public"));
+
+  const pubClient = createClient({ host: "localhost", port: 6379 });
+  const subClient = pubClient.duplicate();
+
+  await Promise.all([pubClient.connect(), subClient.connect()]);
+
+  io.adapter(createAdapter(pubClient, subClient));
+  setupWorker(io);
+
+  io.on("connection", (socket) => {
+    console.log(`connect ${socket.id}`);
+
+    socket.on("disconnect", (reason) => {
+      console.log(`disconnect ${socket.id} due to ${reason}`);
+    });
+  });
+}
