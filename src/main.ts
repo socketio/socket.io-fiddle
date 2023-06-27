f318ec742d650f747cd57c1b012a339e4b9b518e
Damien Arrachequesne <damien.arrachequesne@gmail.com>
2023-06-27 14:38:05 +0200

    feat: add Socket.IO client

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-
+import { io } from "socket.io-client";
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
