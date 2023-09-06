5096d083fc63c3cb0d92c0213c322ee8100567f3
Damien Arrachequesne <damien.arrachequesne@gmail.com>
2023-09-06 10:15:19 +0200

    test

--- server.ts
+++ server.ts
@@ -5,7 +5,11 @@ import { Server } from "socket.io";
 const app = express();
 const server = http.createServer(app);
 
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: true
+  }
+});
 const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + "/public"));
