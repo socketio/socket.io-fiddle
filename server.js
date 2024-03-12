4d92d66a62465df5ac41ce80996a588fc012698e
Damien Arrachequesne <damien.arrachequesne@gmail.com>
2024-03-12 01:58:25 +0100

    feat: simplify fiddle

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
-import { default as express } from "express";
-import { createServer } from "http";
+import { createServer } from "node:http";
+import { readFile } from "node:fs/promises";
 import { Server } from "socket.io";
-
-const app = express();
-const httpServer = createServer(app);
-const io = new Server(httpServer, {});
 
 const port = process.env.PORT || 3000;
 
-app.use(express.static("public"));
+const httpServer = createServer(async (req, res) => {
+  if (req.url !== "/") {
+    res.writeHead(404);
+    res.end("Not found");
+    return;
+  }
+  // reload the file every time
+  const content = await readFile("index.html");
+
+  res.writeHead(200, {
+    "Content-Type": "text/html",
+    "Content-Length": Buffer.byteLength(content),
+  });
+  res.end(content);
+});
+
+const io = new Server(httpServer, {});
 
 io.on("connection", (socket) => {
   console.log(`connect ${socket.id}`);
